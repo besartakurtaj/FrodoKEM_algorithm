7a917ee00a430de8d055775d999cf566f6c641da
Besarta Kurtaj <93934264+besartakurtaj@users.noreply.github.com>
2025-11-02 12:36:36 +0100

    add FrodoCiphertext value object to hold ciphertext bytes

--- src/main/java/com/example/FrodoCiphertext.java
+++ src/main/java/com/example/FrodoCiphertext.java
@@ -1,5 +1,14 @@
 package com.example;
 
 public class FrodoCiphertext {
+  private final byte[] ciphertext;
+
+    public FrodoCiphertext(byte[] ciphertext) {
+        this.ciphertext = ciphertext;
+    }
+
+    public byte[] getCiphertext() {
+        return ciphertext;
+    }
 
 }

a29009ef657fce5e0fcea4f972d59480d2d2a959
Enis Hoxha <enishoxha10@icloud.com>
2025-11-07 14:36:55 +0100

    setup: public and private key pairs

--- src/main/java/com/example/FrodoKeyPair.java
+++ src/main/java/com/example/FrodoKeyPair.java
@@ -1,5 +1,17 @@
 package com.example;
 
-public class FrodoKeyPair {
+import org.bouncycastle.pqc.crypto.frodo.FrodoPrivateKeyParameters;
+import org.bouncycastle.pqc.crypto.frodo.FrodoPublicKeyParameters;
 
+public class FrodoKeyPair {
+    private final FrodoPublicKeyParameters publicKey;
+    private final FrodoPrivateKeyParameters privateKey;
+
+    public FrodoKeyPair(FrodoPublicKeyParameters publicKey, FrodoPrivateKeyParameters privateKey) {
+        this.publicKey = publicKey;
+        this.privateKey = privateKey;
+    }
+
+    public FrodoPublicKeyParameters getPublicKey() { return publicKey; }
+    public FrodoPrivateKeyParameters getPrivateKey() { return privateKey; }
 }
